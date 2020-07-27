634a9217b10268c5aa718420bb39773b22cfb356
Joona Rantanen <joona.rantanen1@gmail.com>
2020-07-27 22:58:43 +0300

    toka osa kokonaan

--- Osa2/puhelinluettelo/src/App.js
+++ Osa2/puhelinluettelo/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import phoneBookService from './phonebook'
+import './index.css'
 
 
 const Person = (props) => {
@@ -11,7 +12,7 @@ const Person = (props) => {
 
         <div>
 
-            {props.name} {props.number} <button onClick={() => props.remove(props.id)}>delete</button>
+            {props.name} {props.number} <button onClick={() => props.remove(props.id, props.name)}>delete</button>
 
 
         </div>
@@ -63,11 +64,38 @@ const PersonForm = (props) => {
     )
 }
 
+const Notification = ({ message }) => {
+    if (message === null) {
+      return null
+    }
+  
+    return (
+      <div className="success">
+        {message}
+      </div>
+    )
+  }
+
+  const ErrorNotification = ({ message }) => {
+    if (message === null) {
+      return null
+    }
+  
+    return (
+      <div className="error">
+        {message}
+      </div>
+    )
+  }
+
 const App = () => {
     const [persons, setPersons] = useState([])
     const [newName, setNewName] = useState('')
     const [newNumber, setNewNumber] = useState('')
     const [filter, setNewFilter] = useState('')
+    const [successMessage, setSuccessMessage] = useState(null)
+    const [errorMessage, setErrorMessage] = useState(null)
+
 
     useEffect(() => {
         phoneBookService
@@ -78,10 +106,18 @@ const App = () => {
             })
     }, [])
 
-    const remove = (id) => {
+    const remove = (id,name) => {
         phoneBookService
             .remove(id)
             .then(data => {
+
+                console.log("DATA", data)
+                setSuccessMessage(
+                    `Deleted ${name} `
+                  )
+                  setTimeout(() => {
+                    setSuccessMessage(null)
+                  }, 5000)
                 setPersons(persons.filter(per => per.id !== id))
             })
     }
@@ -93,7 +129,25 @@ const App = () => {
         phoneBookService
             .update(id, changedPerson)
             .then(data => {
+
+                setSuccessMessage(
+                        `Ubdated ${data.name} `
+                      )
+                      setTimeout(() => {
+                        setSuccessMessage(null)
+                      }, 5000)
                 setPersons(persons.map(per => per.id !== id ? per : data))
+            }).catch(error => {
+
+                setErrorMessage(
+                    `Information of  ${changedPerson.name} has already been removed from the server `
+                  )
+                  setTimeout(() => {
+                    setErrorMessage(null)
+                  }, 5000)
+
+
+                setPersons(persons.filter(n => n.id !== id))
             })
     }
 
@@ -151,6 +205,14 @@ const App = () => {
             phoneBookService
                 .create(nameObject)
                 .then(data => {
+
+                    setSuccessMessage(
+                        `Added ${nameObject.name} `
+                      )
+                      setTimeout(() => {
+                        setSuccessMessage(null)
+                      }, 5000)
+
                     setPersons(persons.concat(data))
 
                 })
@@ -168,6 +230,8 @@ const App = () => {
         <div>
             
             <h2>Phonebook</h2>
+            <Notification message={successMessage} />
+            <ErrorNotification message={errorMessage} />
             <Filter filter={filter} handleFilterChange={handleFilterChange} />
             <h2>add a new</h2>
             <PersonForm addName={addName} newName={newName} handleNameChange={handleNameChange} newNumber={newNumber} handleNumberChange={handleNumberChange} />
